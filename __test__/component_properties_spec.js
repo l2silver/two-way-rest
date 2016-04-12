bbd4399607fe381d3bc507317bf71a479516bf0b
Simplistics <leigh@simplistics.ca>
2016-04-12 17:00:30 -0400

    add component property tests

--- __test__/component_properties_spec.js
+++ __test__/component_properties_spec.js
@@ -4,30 +4,68 @@ import {expect} from 'chai';
 import {fromJS, Map, OrderedMap, List, Seq} from 'immutable';
 import {
 	  setStore
+	, getStore
+	, urlPath
+	, generateTree
+	, getTree
+	, createErrors
+	, createArgs
 	, defaultProperties
 	} from './../lib/componentProperties'
 
-
-//setStore({dispatch: ()=>{}, getState: ()=>{}})
-/*
-createMockComponent(args){
-	var newComponent = {}
-	args.toSeq().map((v,k)=>{
-		if(typeof v === 'function'){
-			return newComponent	
-		}
-	})
+function genMock(defaultProps, props){
+	return Object.assign({}, defaultProps.toJS(), {props: props})
 }
-*/
 
 describe.only('components_properties', ()=>{
+	describe('utility functions', ()=>{
+		it('setGetStore', ()=>{
+			setStore('TheStore');
+			expect(getStore()).to.equal('TheStore');			
+		})
+		describe('generateTree', ()=>{
+			it('generatesTree from array', ()=>{
+				expect(generateTree(['tree'])).to.equal(List(['tree']));
+			})
+			it('generatesTree from location', ()=>{
+				const MComponent = genMock(defaultProperties, {reducer: 'test'})
+				expect(generateTree('tree', MComponent)).to.equal(List(['tree']));
+			})
+			it('generatesTree from complex location', ()=>{
+				const MComponent = genMock(defaultProperties, {reducer: 'test'})
+				expect(generateTree('tree/tree/tree', MComponent)).to.equal(List(['tree', 'tree', 'tree']));
+			})
+		})
+		it('urlPath', ()=>{
+			expect(urlPath(['test'])).to.equal('/test')
+		})
+		describe('getTree', ()=>{
+			it('with start', ()=>{
+				expect(getTree('test', 'http://remoteUrl.com/test/location')).to.equal(List(['location']))
+			})
+			it('without start', ()=>{
+				expect(getTree('tester', 'http://remoteUrl.com/test/location', 'http://remoteUrl.com')).to.equal(List(['test','location']))
+			})
+		})
+		describe('createErrors', ()=>{
+			it('has not errors', ()=>{
+				expect(createErrors()).to.equal('')	
+			})
+			it('has errors', ()=>{
+				expect(createErrors(Map({errors: 'error'})).props.className).to.equal('default-errors')	
+			})
+		})
+		describe('createArgs', ()=>{
+			it('createArgs', ()=>{
+				const MComponent = genMock(defaultProperties, {reducer: 'test', tree: 'tree'})
+				expect(createArgs(MComponent, 'form').get('tree')).to.equal(List(['tree']))
+			})
+		})
+	})
 	describe('defaultProperties', ()=>{
 		it('getTree', ()=>{
-			const MComponent = Object.assign({}, defaultProperties.toJS(), {props: {reducer: 'test'}})
+			const MComponent = genMock(defaultProperties ,{reducer: 'test'})
 			expect(MComponent.getTree({tree:'tests'})).to.equal(List(['tests']))
-		})
-		it('tests apple', ()=>{
-
 		})
 	})
 })
